ed0cfafd0fdb4f6bef5e7c4593250128785721cc
Aaron Klinker <aaronklinker1@gmail.com>
2017-03-15 16:26:52 -0500

    Initial commit

---
+++ src/com/klinker/platformer2d/R.java
@@ -0,0 +1,119 @@
+package com.klinker.platformer2d;
+
+public class R {
+    public static class levels {
+        public static final String LEVEL_DESIGN = "res/levels/level-design.txt";
+        public static final String LEVELS_TO_BITMAP = "res/levels/levels-to-bitmap.txt";
+        public static final String W01_L01 = "res/levels/w01-l01.lvl";
+        public static final String W01_L01S = "res/levels/w01-l01s.lvl";
+        public static final String W01_LXX = "res/levels/w01-lxx.lvl";
+        public static final String W02_LXX = "res/levels/w02-lxx.lvl";
+        public static final String W03_LXX = "res/levels/w03-lxx.lvl";
+        public static final String W04_LXX = "res/levels/w04-lxx.lvl";
+    }
+
+    public static class shaders {
+        public static class frag {
+            public static final String BACKGROUND = "res/shaders/frag/background.frag";
+            public static final String BASIC_ALPHA = "res/shaders/frag/basic-alpha.frag";
+            public static final String BASIC = "res/shaders/frag/basic.frag";
+            public static final String MOVE = "res/shaders/frag/move.frag";
+            public static final String PIPE = "res/shaders/frag/pipe.frag";
+        }
+
+        public static final String SHADERS = "res/shaders/shaders.iml";
+
+        public static class vert {
+            public static final String BACKGROUND = "res/shaders/vert/background.vert";
+            public static final String BASIC = "res/shaders/vert/basic.vert";
+            public static final String MOVE = "res/shaders/vert/move.vert";
+            public static final String PIPE = "res/shaders/vert/pipe.vert";
+        }
+    }
+
+    public static class textures {
+        public static class bg {
+            public static final String B01_00 = "res/textures/bg/01-00.png";
+            public static final String B01_01 = "res/textures/bg/01-01.png";
+            public static final String B01_02 = "res/textures/bg/01-02.png";
+            public static final String B01 = "res/textures/bg/01.png";
+            public static final String B02 = "res/textures/bg/02.png";
+            public static final String B03 = "res/textures/bg/03.png";
+            public static final String B07 = "res/textures/bg/07.png";
+            public static final String B09 = "res/textures/bg/09.png";
+        }
+
+        public static final String BG = "res/textures/bg.jpeg";
+        public static final String BIRD = "res/textures/bird.png";
+
+        public static class character {
+            public static class hero01 {
+                public static final String BODY = "res/textures/character/hero01/body.png";
+            }
+        }
+
+        public static final String PIPE = "res/textures/pipe.png";
+
+        public static class tiles {
+            public static final String T01_01 = "res/textures/tiles/01-01.png";
+            public static final String T01_02 = "res/textures/tiles/01-02.png";
+            public static final String T01_03 = "res/textures/tiles/01-03.png";
+            public static final String T01_04 = "res/textures/tiles/01-04.png";
+            public static final String T01_05 = "res/textures/tiles/01-05.png";
+            public static final String T01_06 = "res/textures/tiles/01-06.png";
+            public static final String T01_07 = "res/textures/tiles/01-07.png";
+            public static final String T01_08 = "res/textures/tiles/01-08.png";
+            public static final String T01_09 = "res/textures/tiles/01-09.png";
+            public static final String T01_0A = "res/textures/tiles/01-0A.png";
+            public static final String T01_0B = "res/textures/tiles/01-0B.png";
+            public static final String T01_0C = "res/textures/tiles/01-0C.png";
+            public static final String T01_0D = "res/textures/tiles/01-0D.png";
+            public static final String T02_01 = "res/textures/tiles/02-01.png";
+            public static final String T02_02 = "res/textures/tiles/02-02.png";
+            public static final String T02_03 = "res/textures/tiles/02-03.png";
+            public static final String T02_04 = "res/textures/tiles/02-04.png";
+            public static final String T02_05 = "res/textures/tiles/02-05.png";
+            public static final String T02_06 = "res/textures/tiles/02-06.png";
+            public static final String T02_07 = "res/textures/tiles/02-07.png";
+            public static final String T02_08 = "res/textures/tiles/02-08.png";
+            public static final String T02_09 = "res/textures/tiles/02-09.png";
+            public static final String T02_0A = "res/textures/tiles/02-0A.png";
+            public static final String T02_0B = "res/textures/tiles/02-0B.png";
+            public static final String T02_0C = "res/textures/tiles/02-0C.png";
+            public static final String T02_0D = "res/textures/tiles/02-0D.png";
+            public static final String T03_01 = "res/textures/tiles/03-01.png";
+            public static final String T03_02 = "res/textures/tiles/03-02.png";
+            public static final String T03_03 = "res/textures/tiles/03-03.png";
+            public static final String T03_04 = "res/textures/tiles/03-04.png";
+            public static final String T03_05 = "res/textures/tiles/03-05.png";
+            public static final String T03_06 = "res/textures/tiles/03-06.png";
+            public static final String T03_07 = "res/textures/tiles/03-07.png";
+            public static final String T03_08 = "res/textures/tiles/03-08.png";
+            public static final String T03_09 = "res/textures/tiles/03-09.png";
+            public static final String T03_0A = "res/textures/tiles/03-0A.png";
+            public static final String T03_0B = "res/textures/tiles/03-0B.png";
+            public static final String T03_0C = "res/textures/tiles/03-0C.png";
+            public static final String T03_0D = "res/textures/tiles/03-0D.png";
+            public static final String T04_01 = "res/textures/tiles/04-01.png";
+            public static final String T04_02 = "res/textures/tiles/04-02.png";
+            public static final String T04_03 = "res/textures/tiles/04-03.png";
+            public static final String T04_04 = "res/textures/tiles/04-04.png";
+            public static final String T04_05 = "res/textures/tiles/04-05.png";
+            public static final String T04_06 = "res/textures/tiles/04-06.png";
+            public static final String T04_07 = "res/textures/tiles/04-07.png";
+            public static final String T04_08 = "res/textures/tiles/04-08.png";
+            public static final String T04_09 = "res/textures/tiles/04-09.png";
+            public static final String T04_0A = "res/textures/tiles/04-0A.png";
+            public static final String T04_0B = "res/textures/tiles/04-0B.png";
+            public static final String T04_0C = "res/textures/tiles/04-0C.png";
+            public static final String T04_0D = "res/textures/tiles/04-0D.png";
+        }
+
+        public static class worlds {
+            public static final String BLUE = "res/textures/worlds/blue.png";
+            public static final String BROWN = "res/textures/worlds/brown.png";
+            public static final String PURPLE = "res/textures/worlds/purple.png";
+            public static final String RED = "res/textures/worlds/red.png";
+        }
+    }
+}
