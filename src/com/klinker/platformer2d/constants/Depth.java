9ee02036591cad485b573a060102e693119e0b8b
Aaron Klinker <aaronklinker1@gmail.com>
2017-03-15 17:39:17 -0500

    Refactored directory from physics to constants

--- src/com/klinker/platformer2d/constants/Depth.java
+++ src/com/klinker/platformer2d/constants/Depth.java
@@ -1,4 +1,4 @@
-package com.klinker.platformer2d.physics;
+package com.klinker.platformer2d.constants;
 
 /**
  * Smaller Depth = first to be drawn.
@@ -11,7 +11,10 @@ public class Depth {
     public static final float BACKGROUND_BOTTOM = -1f;
     public static final float BACKGROUND_MIDDLE = -0.9f;
     public static final float BACKGROUND_TOP = -0.8f;
-    public static final float ENEMY = -0.1f;
+    public static final float ENEMY_TOP = -0.4f;
+    public static final float ENEMY_MIDDLE = -0.3f;
+    public static final float ENEMY_BOTTOM = -0.2f;
+    public static final float PLAYER_GHOST = -0.1f;
     public static final float PLAYER = 0.0f;
 
 }
