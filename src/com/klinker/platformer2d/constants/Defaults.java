084beec03364b7117a0a6d7754afb08bc612753b
Aaron Klinker <aaronklinker1@gmail.com>
2017-03-24 21:20:32 -0500

    Initial commit.

---
+++ src/com/klinker/platformer2d/constants/Defaults.java
@@ -0,0 +1,10 @@
+package com.klinker.platformer2d.constants;
+
+public class Defaults {
+
+    public static final int TILE_WIDTH = 28; // tiles
+    public static final float ASPECT_RATIO = 9 / 16f; // height / width
+    public static final int FRAME_RATE = 60; // fps
+    public static final int WINDOW_WIDTH = 1280; // pixels
+
+}
