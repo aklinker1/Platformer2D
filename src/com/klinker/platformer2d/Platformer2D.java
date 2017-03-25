33bbadaa4ae06a35550236ea2c3f7d1730531d00
Aaron Klinker <aaronklinker1@gmail.com>
2017-03-24 21:26:04 -0500

    Modified static variables and moved them to Engine.

--- src/com/klinker/platformer2d/Platformer2D.java
+++ src/com/klinker/platformer2d/Platformer2D.java
@@ -5,31 +5,61 @@ package com.klinker.platformer2d;
 import com.klinker.engine2d.Engine;
 import com.klinker.engine2d.maths.Size;
 import com.klinker.engine2d.maths.Vector2f;
+import com.klinker.platformer2d.constants.Defaults;
 import com.klinker.platformer2d.scenes.Level;
+import com.klinker.platformer2d.utils.Settings;
 
 
 public class Platformer2D extends Engine {
 
-    public static final int TILE_WIDTH = 28;
-    public static int frameRate = 60;
-    public static final Vector2f TILE_COUNT = new Vector2f(TILE_WIDTH, (float) TILE_WIDTH * 9f / 16f);
-    public static Size<Integer> SIZE = new Size<>(1280, 720);
+    private static final String SETTINGS_PATH = "settings.pref";
+
+    public static Vector2f tileCounts;
+
+    private Settings settings;
 
     public static void main(String[] args) {
         Platformer2D platformer = new Platformer2D();
         platformer.setStyle(Style.SMOOTH);
-        platformer.setSize(SIZE);
-        platformer.setFrameRate(frameRate);
         platformer.start();
     }
 
     public Platformer2D() {
+        settings = initializeSettings();
         setScene(new Level(R.levels.W02_LXX));
+    }
+
+    @Override
+    protected void onFinish() {
+        // Save settings
+        if (settings != null) settings.outputToFile(SETTINGS_PATH);
     }
 
     @Override
     public String getWindowTitle() {
         return "Platformer 2D";
+    }
+
+    private Settings initializeSettings() {
+        Settings settings = new Settings(SETTINGS_PATH);
+
+        // check to see if a settings file exists, and initialize the defaults because it's the first open
+        if (!settings.hasData()) {
+            settings = new Settings();
+            settings.put(Settings.KEY_FRAME_RATE, Defaults.FRAME_RATE);
+            settings.put(Settings.KEY_ASPECT_RATIO, Defaults.ASPECT_RATIO);
+            settings.put(Settings.KEY_TILE_WIDTH, Defaults.TILE_WIDTH);
+            settings.put(Settings.KEY_WINDOW_WIDTH, Defaults.WINDOW_WIDTH);
+        }
+
+        frameRate = settings.getInt(Settings.KEY_FRAME_RATE);
+        float aspectRatio = settings.getFloat(Settings.KEY_ASPECT_RATIO);
+        int windowWidth = settings.getInt(Settings.KEY_WINDOW_WIDTH);
+        windowSize = new Size<>(windowWidth, (int) (windowWidth * aspectRatio));
+        int tileWidth = settings.getInt(Settings.KEY_TILE_WIDTH);
+        tileCounts = new Vector2f(tileWidth, aspectRatio * tileWidth);
+
+        return settings;
     }
 
 }
