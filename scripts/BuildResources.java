a3b0f6940d024200cedc3257a7dfb6873c4cd2ef
Aaron Klinker <aaronklinker1@gmail.com>
2017-03-25 18:51:40 -0500

    Reimplemented fonts and added a todo for strings

--- scripts/BuildResources.java
+++ scripts/BuildResources.java
@@ -31,7 +31,12 @@ public class BuildResources {
         // set up the res loop
         resRoot = new File("../" + resDir);
         for (File file : resRoot.listFiles()) {
-           if (file.isDirectory()) fileBody += createGeneral(file, 1);
+            if (file.isDirectory() && file.getName().equalsIgnoreCase("fonts"))
+                fileBody += createFonts(file);
+            //else if (file.isDirectory() && file.getName().equalsIgnoreCase("strings"))
+            //    fileBody += createStrings(file);
+            else if (file.isDirectory())
+                fileBody += createGeneral(file, 1);
         }
         fileBody += "}";
 
@@ -64,6 +69,7 @@ public class BuildResources {
         return body;
     }
 
+    // TODO: 3/25/2017 Finish
     private String createStrings(File stringDir) {
         String body = "    public static class strings {\n" +
                 "        public static String getString(String res)";
@@ -79,6 +85,18 @@ public class BuildResources {
             }
         }
         return "";
+    }
+
+    private String createFonts(File fontDir) {
+        String body = "    public static class fonts {\n";
+        for (File font : fontDir.listFiles()) {
+            if (font.isDirectory()) {
+                String varName = font.getName().toUpperCase().replace('-', '_');
+                body += "        public static final String " + varName +
+                        " = \"" + font.getPath().replace('\\', '/').replace("../", "") + "\";\n";
+            }
+        }
+        return body + "    }\n";
     }
 
 }
