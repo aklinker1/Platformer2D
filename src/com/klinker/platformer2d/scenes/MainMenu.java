dfa1b033c257d00a1a370b4ec0f9e2f81d693b52
Aaron Klinker <aaronklinker1@gmail.com>
2017-03-25 18:53:37 -0500

    switched from labels to views

--- src/com/klinker/platformer2d/scenes/MainMenu.java
+++ src/com/klinker/platformer2d/scenes/MainMenu.java
@@ -1,6 +1,7 @@
 package com.klinker.platformer2d.scenes;
 
 import com.klinker.engine2d.graphics.Shader;
+import com.klinker.engine2d.graphics.Texture;
 import com.klinker.engine2d.gui.TextView;
 import com.klinker.engine2d.gui.View;
 import com.klinker.engine2d.maths.Size;
@@ -14,16 +15,16 @@ import java.util.LinkedList;
 
 public class MainMenu extends Menu {
 
-    private SimpleSprite background;
+    private View background;
 
     @Override
     public void init() {
         super.init();
-        background = new SimpleSprite(
-                new Vector2f(-50f, -50f),
-                new Size<Float>(100f, 100f),
-                R.textures.bg.B04_00
+        background = new View(
+                new Vector3f(-50f, -50f, Depth.BACKGROUND_BACK),
+                new Size<Float>(100f, 100f)
         );
+        background.setBackgroundTexture(R.textures.bg.MAIN_MENU);
     }
 
     @Override
@@ -44,9 +45,9 @@ public class MainMenu extends Menu {
     }
 
     @Override
-    protected void initializeNavigation(LinkedList<View> labels) {
-        labels.add(new TextView(
-                "H",
+    protected void initializeViews(LinkedList<View> views) {
+        views.add(new TextView(
+                "Hello World!",
                 new Vector3f(0f, 0f, Depth.TILE_FRONT),
                 R.fonts.ROBOTO
         ));
