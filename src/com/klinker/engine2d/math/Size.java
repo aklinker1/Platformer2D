641864045b3dafbe9891ff2380a5e4591f41c8a5
Aaron Klinker <aaronklinker1@gmail.com>
2017-03-25 22:22:56 -0500

    Refactored parent package name

---
+++ src/com/klinker/engine2d/math/Size.java
@@ -0,0 +1,22 @@
+package com.klinker.engine2d.math;
+
+public class Size<T extends Number> {
+
+    public T width;
+    public T height;
+
+    public Size(T width, T height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public Size(T size) {
+        this.width = size;
+        this.height = size;
+    }
+
+    @Override
+    public String toString() {
+        return "Size(" + width + "x" + height + ")";
+    }
+}
