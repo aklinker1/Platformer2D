2a8c0aff418eb72bc68bacec2be9d4b5c36d2bdc
Aaron Klinker <aaronklinker1@gmail.com>
2017-03-27 09:32:55 -0500

    Added basic navigation, buggy

--- src/com/klinker/platformer2d/scenes/MainMenu.java
+++ src/com/klinker/platformer2d/scenes/MainMenu.java
@@ -1,41 +1,29 @@
 package com.klinker.platformer2d.scenes;
 
 import com.klinker.engine2d.draw.WrapWidthSprite;
+import com.klinker.engine2d.gui.Button;
 import com.klinker.engine2d.math.Vector2f;
 import com.klinker.engine2d.opengl.Shader;
 import com.klinker.engine2d.gui.TextView;
 import com.klinker.engine2d.gui.View;
 import com.klinker.engine2d.math.Size;
-import com.klinker.engine2d.utils.Log;
 import com.klinker.platformer2d.R;
+import com.klinker.platformer2d.constants.Depth;
 import com.klinker.platformer2d.sprite.SimpleSprite;
+import com.klinker.platformer2d.utils.MenuNavigation;
 
 import java.util.LinkedList;
 
 public class MainMenu extends Menu {
 
-    private View background;
+    private MenuNavigation<Button> navigation;
 
-    @Override
-    public void init() {
-        super.init();
-        background = new View(
-                new Vector2f(-PROJ_SIZE.width / 2f, -PROJ_SIZE.height / 2f),
-                new Size<Float>(PROJ_SIZE.width, PROJ_SIZE.height)
-        );
-        background.setBackgroundTexture(R.textures.bg.MAIN_MENU);
-    }
-
-    @Override
-    public void update() {
-        background.update();
-        super.update();
-    }
-
-    @Override
-    public void render() {
-        background.render();
-        super.render();
+    public MainMenu() {
+        this.navigation = new MenuNavigation<>();
+        navigation.setOnItemSelectedListener((Button oldButton, Button newButton) -> {
+            if (oldButton != null) oldButton.setState(View.State.DEFAULT);
+            if (newButton != null) newButton.setState(View.State.SELECTED);
+        });
     }
 
     @Override
@@ -45,37 +33,84 @@ public class MainMenu extends Menu {
 
     @Override
     protected void initializeViews(LinkedList<View> views) {
+        View background = new View(
+                new Vector2f(-PROJ_SIZE.width / 2f, -PROJ_SIZE.height / 2f),
+                Depth.BACKGROUND_BACK,
+                new Size<Float>(PROJ_SIZE.width, PROJ_SIZE.height)
+        );
+        background.setBackgroundTexture(R.textures.bg.MAIN_MENU);
+        views.add(background);
+
         TextView version = new TextView(
                 "Version 0.0.1 Alpha",
                 3f,
                 new Vector2f(PROJ_SIZE.width / 2f - 2f,-PROJ_SIZE.height / 2f + 2f),
                 R.fonts.ROBOTO
         );
-        Log.d("position = " + version.getPosition());
+        version.setTextSize(3f);
         version.setTextColor(0x6b000000);
         version.setHorAlignment(TextView.Alignment.RIGHT);
         views.add(version);
+
+        Button storyMode = new Button(R.strings.STORY_MODE,
+                new Size<Float>(35f, 8f),
+                new Vector2f(25f, 25f),
+                R.fonts.ROBOTO
+        );
+        storyMode.setTextSize(4.5f);
+        storyMode.setState(View.State.SELECTED);
+        views.add(storyMode);
+
+        Button muliplayer = new Button(R.strings.MULTIPLAYER,
+                new Size<Float>(35f, 8f),
+                storyMode.getPosition().translate(0f, -12f),
+                R.fonts.ROBOTO
+        );
+        muliplayer.setTextSize(4.5f);
+        views.add(muliplayer);
+
+        navigation.insertNode(storyMode, null, null, null, muliplayer);
+        navigation.insertNode(muliplayer, null, storyMode, null, null);
+        navigation.select(storyMode);
+
+        // TODO: 3/26/2017 Add grid?
+
+        View vertDivider = new View(new Vector2f(0, 0), Depth.BACKGROUND_FRONT, new Size<Float>(0.2f, PROJ_SIZE.height));
+        vertDivider.setHorAlignment(View.Alignment.CENTER);
+        vertDivider.setVertAlignment(View.Alignment.CENTER);
+        vertDivider.setBackgroundTexture(R.textures.bg.B03_00);
+        views.add(vertDivider);
+
+        View horDivider = new View(new Vector2f(0, 0), Depth.BACKGROUND_FRONT, new Size<Float>(PROJ_SIZE.width, 0.2f));
+        horDivider.setHorAlignment(View.Alignment.CENTER);
+        horDivider.setVertAlignment(View.Alignment.CENTER);
+        horDivider.setBackgroundTexture(R.textures.bg.B03_00);
+        views.add(horDivider);
 
     }
 
     @Override
     public void onLeftPress() {
-        Log.d("Left");
+        Button left = navigation.getLeft();
+        if (left != null) navigation.select(left);
     }
 
     @Override
     public void onUpPress() {
-        Log.d("Up");
+        Button up = navigation.getUp();
+        if (up != null) navigation.select(up);
     }
 
     @Override
     public void onRightPress() {
-        Log.d("Right");
+        Button right = navigation.getRight();
+        if (right != null) navigation.select(right);
     }
 
     @Override
     public void onDownPress() {
-        Log.d("Down");
+        Button down = navigation.getDown();
+        if (down != null) navigation.select(down);
     }
 
 }
