f9c2153f9ed60c9f24d864d2dd9ed26d7157eafc
Aaron Klinker <aaronklinker1@gmail.com>
2017-03-30 10:11:34 -0500

    Updated zip builder. Now removes any old files from zip, added a readme.txt, and used a .sh instead of .bat to support all OS's

--- scripts/CreateGameZip.bat
+++ scripts/CreateGameZip.bat
@@ -4,8 +4,9 @@ robocopy ../res ../out/artifacts/Platformer2D_jar/res/ /E
 robocopy . ../out/artifacts/Platformer2D_jar/ Debug.bat
 ECHO 2/%steps% - Zipping Game Contents.
 SET zipper="C:\Program Files\7-Zip\7z.exe"
-cd ../out/artifacts/Platformer2D_jar/
-del "../../../Platformer2D.zip"
+cd ../
+del Platformer2D.zip
+cd out/artifacts/Platformer2D_jar/
 %zipper% a "../../../Platformer2D.zip" "res" "Platformer2D.jar" "../../../out/zip/Debug.sh" "../../../out/zip/README.txt"
 ECHO 3/%steps% - Cleaning up.
 rd res /s /q
