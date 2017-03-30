3b0cd784147cecf28ea00ccfc6c58f43c22747d5
Aaron Klinker <aaronklinker1@gmail.com>
2017-03-30 10:04:54 -0500

    updated project

--- scripts/CreateGameZip.bat
+++ scripts/CreateGameZip.bat
@@ -4,10 +4,9 @@ robocopy ../res ../out/artifacts/Platformer2D_jar/res/ /E
 robocopy . ../out/artifacts/Platformer2D_jar/ Debug.bat
 ECHO 2/%steps% - Zipping Game Contents.
 SET zipper="C:\Program Files\7-Zip\7z.exe"
-cd ../
-cd out/artifacts/Platformer2D_jar/
+cd ../out/artifacts/Platformer2D_jar/
 del "../../../Platformer2D.zip"
-%zipper% a "../../../Platformer2D.zip" "res" "Platformer2D.jar" "Debug.bat"
+%zipper% a "../../../Platformer2D.zip" "res" "Platformer2D.jar" "../../../out/zip/Debug.sh" "../../../out/zip/README.txt"
 ECHO 3/%steps% - Cleaning up.
 rd res /s /q
 ECHO Finished!
