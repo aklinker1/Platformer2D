97600df265822b9bd62cc054a3e4a453c6c79e30
Aaron Klinker <aaronklinker1@gmail.com>
2017-04-07 12:42:53 -0500

    Modified Tile to be abstract, so I have to implement a class for each tile.

--- src/com/klinker/platformer2d/sprite/tiles/Tile.java
+++ src/com/klinker/platformer2d/sprite/tiles/Tile.java
@@ -1,58 +1,45 @@
 package com.klinker.platformer2d.sprite.tiles;
 
-import com.klinker.engine2d.draw.Camera;
-import com.klinker.engine2d.draw.Sprite;
+import com.klinker.engine2d.draw.SimpleSprite;
 import com.klinker.engine2d.math.Size;
 import com.klinker.engine2d.math.Vector2f;
 import com.klinker.engine2d.math.Vector3f;
 import com.klinker.engine2d.opengl.Shader;
-import com.klinker.engine2d.opengl.Texture;
-import com.klinker.engine2d.utils.CollisionBox;
 import com.klinker.engine2d.utils.Log;
 import com.klinker.platformer2d.R;
-import com.klinker.platformer2d.constants.Depth;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
-public class Tile extends Sprite {
+public abstract class Tile extends SimpleSprite {
+
+    public static final String[] tileMapping = new String[]{
+
+    };
 
     private int world;
     private int tile;
 
     public static final Shader SHADER = new Shader(R.shaders.vert.BASIC, R.shaders.frag.BASIC);
 
+    public Tile(Vector3f position, Size<Float> size, String textRes) {
+        super(position, size, textRes);
+    }
+
     public static Tile newInstance(Vector2f position, int world, int tile) {
-        String klassName = String.format("com.klinker.platformer2d.tile.T%02X", tile);
+        String klassName = "com.klinker.platformer2d.tile." + tileMapping[tile];
         try {
             Class<?> klass = Class.forName(klassName);
-            Constructor<?> constructor = klass.getConstructor(String.class);
-            return (Tile) constructor.newInstance(new Object[]{ world });
+            Constructor<?> constructor = klass.getConstructor(Vector2f.class, int.class);
+            return (Tile) constructor.newInstance(position, world);
         } catch (ClassNotFoundException e) {
-            return new Tile(new Vector3f(position.x, position.y, Depth.TILE), world, tile);
+            Log.d("Tile not created: " + tile + ", " + tileMapping[tile]);
         } catch (NoSuchMethodException e) {
-            Log.e("Could not find ", e);
+            Log.e("Could not find constructor for " + tileMapping[tile], e);
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            Log.e("Error calling new instance", e);
+            Log.e("Error calling constructor for " + tileMapping[tile], e);
         }
         return null;
-    }
-
-    protected Tile(Vector3f position, int world, int tile) {
-        super(
-                position,
-                new Size<>(1f, 1f),
-                new Texture(String.format("res/textures/tiles/%02X-%02X.png", world, tile)),
-                SHADER
-        );
-        this.world = world;
-        this.tile = tile;
-        setCollision(new CollisionBox(CollisionBox.Shape.RECTANGLE, size, new Vector2f(), this.position));
-    }
-
-    @Override
-    public void update(Camera camera) {
-
     }
 
 }
